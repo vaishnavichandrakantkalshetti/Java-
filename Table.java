b3990dada4e68ffefdd70989bf112b82267e5fd3
vaishnavichandrakantkalshetti <100556285+vaishnavichandrakantkalshetti@users.noreply.github.com>
2023-11-13 20:04:29 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Table
+{
+	public static void main(String [] args)
+	{
+	Scanner s= new Scanner(System.in);
+	
+	int i=1,j=1;
+	
+	//int num=s.nextInt();
+	
+	for(i=1;i<=10;i++)
+	{ 
+		for(j=1;j<=10;j++)
+		{		System.out.println(i*j);
+		}
+	}
+	}
+}
